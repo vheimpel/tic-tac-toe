872535a469c7ef4bb0f8ff03b10a67cbcc1c4a7b
Val Heimpel <vheimpel@gmail.com>
2018-11-03 15:02:11 -0400

    add values to squares

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
@@ -14,7 +14,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
@@ -59,6 +59,9 @@ class Game extends React.Component {
   }
 }
 
-// export default App;
+// ========================================
 
-
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
